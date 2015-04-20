64e77b17b6f3a42cb0ff9a7d09944d5a3d1e67c8
Alexey Vasyukov <vasyukov@gmail.com>
2015-04-20 15:20:33 +0300

    Add basic example of gprof usage

---
+++ gprof/gprof-demo.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+void new_func1(void)
+{
+	printf("\n Inside new_func1()\n");
+	int i;
+
+	for(i = 0; i < 0xffffffee; i++);
+
+	return;
+}
+
+void func1(void)
+{
+	printf("\n Inside func1 \n");
+	int i;
+
+	for(i = 0; i < 0xffffffff; i++);
+
+	new_func1();
+
+	return;
+}
+
+static void func2(void)
+{
+	printf("\n Inside func2 \n");
+	int i;
+
+	for(i = 0; i < 0xffffffaa; i++);
+
+	return;
+}
+
+int main(void)
+{
+	printf("\n Inside main()\n");
+	int i;
+
+	for(i = 0; i < 0xffffff; i++);
+
+	func1();
+	func2();
+
+	return 0;
+}
