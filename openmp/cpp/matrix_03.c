27363dcab6e4b93c6318fe91f1add79493ddc081
Alexey Vasyukov <vasyukov@gmail.com>
2020-04-08 19:16:52 +0300

    OpenMP samples

---
+++ openmp/cpp/matrix_03.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <time.h>
+
+
+#define N 2048
+
+float a[N][N];
+float b[N][N];
+float c[N][N];
+
+
+void init_matrix() {
+    unsigned long long int i, j;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            a[i][j] = random()/RAND_MAX;
+            b[i][j] = random()/RAND_MAX;
+        };
+    };
+};
+
+void mult() {
+    unsigned long long int i, j, k;
+   
+    #pragma omp parallel
+    { 
+        for (k = 0; k < N; k++) {
+            for (i = 0; i < N; i++) {
+                #pragma omp for 
+                for (j = 0; j < N; j++) {
+                    c[i][j] = c[i][j] + a[i][k]*b[k][j];
+                };
+            };
+        };
+    }
+};
+
+int main(int argc, char **argv) {
+    struct timespec begin, end;
+    unsigned long int delta_ms;
+
+    init_matrix();
+
+    mult();
+
+    return EXIT_SUCCESS;
+}
---
+++ openmp/cpp/matrix_numpy_2.py
@@ -0,0 +1,19 @@
+from random import random
+import numpy as np
+
+N = 2048
+
+a = np.zeros((N, N))
+b = np.zeros((N, N))
+c = np.zeros((N, N))
+
+def init_matrix():
+    a = np.random.rand(N, N)
+    b = np.random.rand(N, N)
+
+def mult():
+    c = a.dot(b)
+
+if __name__ == '__main__':
+    init_matrix()
+    mult()
